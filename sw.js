64f0c89770fdd32c6bd1026b231dd40e52fabf9d
mrearthpig-au <mrearthpig@gmail.com>
2026-05-09 22:01:39 +0930

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,43 @@
-const CACHE_NAME = 'ilol-calculator-v1';
-const urlsToCache = [
+/* --- Service Worker for ILOL & Impound Date Calculator --- */
+
+const CACHE_NAME = 'date-calc-v1';
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json',
-  './ILOL.png'
+  './ILOL.png',
+  './icon-512.png',
+  './manifest.json'
 ];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache and adding assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return the cached file, or perform a network fetch if not in cache
+      return response || fetch(event.request);
+    })
+  );
+});
